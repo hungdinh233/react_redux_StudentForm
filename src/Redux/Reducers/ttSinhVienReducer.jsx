e19afab84aee62ecb203afb8b96a56a408b9b0cd
hungdinh233 <dinhtienhung233@gmail.com>
2022-08-25 23:15:14 +0700

    update function

--- src/Redux/Reducers/ttSinhVienReducer.jsx
+++ src/Redux/Reducers/ttSinhVienReducer.jsx
@@ -20,21 +20,90 @@ const defaultState = {
       email: "tuan_tran@gmail.com",
     },
   ],
+  validErr: {
+    maSV: "",
+    tenSV: "",
+    soDienThoai: "",
+    email: "",
+  },
 };
 
 export const ttSinhVienReducer = (state = defaultState, action) => {
   switch (action.type) {
-    case "INPUT_RECIVER": {
-      let { id, value } = action.payload;
+    case "HANDLE_INPUT": {
+      let { id, value, dataType } = action.payload;
+      console.log(id, value)
       let inpSinhVien = { ...state.sinhVien };
       inpSinhVien[id] = value;
       state.sinhVien = inpSinhVien;
+      //------Thông báo tính đầy đủ khi điền Input------
+      let validError = { ...state.validErr };
+      let errMess = "";
+      if (value.trim() === "") {
+        errMess = "Không để trống phần thông tin này!";
+      } else {
+        if (dataType === "id") {
+          let regexNumberId = /^[0-9]*$/;
+          if (!regexNumberId.test(value)) {
+            errMess = "Mã sinh viên phải là số!";
+          }
+        }
+        if (dataType === "name") {
+          let regexName =
+            /[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s]+$/;
+          if (!regexName.test(value)) {
+            errMess = "Tên sinh viên phải là chữ có/không dấu";
+          }
+        }
+        if (dataType === "tel") {
+          let regexTel =
+            /^(0|84)(2(0[3-9]|1[0-6|8|9]|2[0-2|5-9]|3[2-9]|4[0-9]|5[1|2|4-9]|6[0-3|9]|7[0-7]|8[0-9]|9[0-4|6|7|9])|3[2-9]|5[5|6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])([0-9]{7})$/;
+          if (!regexTel.test(value)) {
+            errMess = "Số điện thoại không hợp lệ!";
+          }
+        }
+        if (dataType === "email") {
+          let regexMail = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+          if (!regexMail.test(value)) {
+            errMess = "Email không hợp lệ!";
+          }
+        }
+      }
+      validError[id] = errMess;
+      //console.log("validErr", validError);
+      state.validErr = validError;
+      //-----------------
       return { ...state };
     }
     case "HANDLE_SUBMIT": {
+      //------Kiểm tra tính đầy đủ khi submit và Xử lý validation------
+      let valid = true;
+      let baoLoi = { ...state.validErr };
+      for (let key in baoLoi) {
+        if (baoLoi[key] !== "") {
+          valid = false;
+          break;
+        }
+      }
+      for (let key in state.sinhVien) {
+        if (state.sinhVien[key] === "") {
+          baoLoi[key] = "Không để trống phần thông tin này!";
+          valid = false;
+          //   break;
+        }
+      }
+      if (!valid) {
+        console.log("err", baoLoi);
+        state.validErr = baoLoi;
+        alert("Dữ liệu không hợp lệ!");
+        return { ...state };
+      }
+      //-------------------------------------------------
       let danhSachSinhVienUpdate = [...state.danhSachSinhVien];
       danhSachSinhVienUpdate.push(state.sinhVien);
       state.danhSachSinhVien = danhSachSinhVienUpdate;
+      console.log(valid);
+      alert("Thêm sinh viên thành công!");
       return { ...state };
     }
     case "HANDLE_DELETE": {
--- src/ThongTinSinhVien/InputFormTTSV.jsx
+++ src/ThongTinSinhVien/InputFormTTSV.jsx
@@ -4,11 +4,13 @@ import { connect } from "react-redux";
 class InputFormTTSV extends Component {
   getInfo = (e) => {
     let { id, value } = e.target;
+    let dataType = e.target.getAttribute("data-type")
     const action = {
-      type: "INPUT_RECIVER",
+      type: "HANDLE_INPUT",
       payload: {
         id: id,
         value: value,
+        dataType: dataType
       },
     };
     this.props.dispatch(action);
@@ -21,6 +23,7 @@ class InputFormTTSV extends Component {
     this.props.dispatch(action);
   };
   render() {
+    let { err } = this.props;
     return (
       <div className="container">
         <div className="card">
@@ -28,16 +31,20 @@ class InputFormTTSV extends Component {
             <h2>Thông tin sinh viên</h2>
           </div>
           <div className="card-body">
-            <div className="row d-flex justify-content-between">
+            <div className="row d-flex justify-content-between mb-3">
               <div className="col-6 d-flex flex-column">
                 <span>Mã sinh viên</span>
                 <input
                   type="text"
                   name=""
                   id="maSV"
+                  data-type = "id"
                   className="form-control"
                   onChange={this.getInfo}
                 />
+                <span className="text-danger" style={{ fontSize: 14 }}>
+                  {err.maSV}
+                </span>
               </div>
               <div className="col-6 d-flex flex-column">
                 <span>Họ và tên</span>
@@ -45,21 +52,29 @@ class InputFormTTSV extends Component {
                   type="text"
                   name=""
                   id="tenSV"
+                  data-type = "name"
                   className="form-control"
                   onChange={this.getInfo}
                 />
+                <span className="text-danger" style={{ fontSize: 14 }}>
+                  {err.tenSV}
+                </span>
               </div>
             </div>
-            <div className="row d-flex justify-content-between">
+            <div className="row d-flex justify-content-between mb-3">
               <div className="col-6 d-flex flex-column">
                 <span>Số điện thoại</span>
                 <input
                   type="number"
                   name=""
                   id="soDienThoai"
+                  data-type = "tel"
                   className="form-control"
                   onChange={this.getInfo}
                 />
+                <span className="text-danger" style={{ fontSize: 14 }}>
+                  {err.soDienThoai}
+                </span>
               </div>
               <div className="col-6 d-flex flex-column">
                 <span>Email</span>
@@ -67,9 +82,13 @@ class InputFormTTSV extends Component {
                   type="text"
                   name=""
                   id="email"
+                  data-type ="email"
                   className="form-control"
                   onChange={this.getInfo}
                 />
+                <span className="text-danger" style={{ fontSize: 14 }}>
+                  {err.email}
+                </span>
               </div>
             </div>
           </div>
@@ -88,6 +107,8 @@ class InputFormTTSV extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  err: state.ttSinhVienReducer.validErr,
+});
 
 export default connect(mapStateToProps)(InputFormTTSV);
